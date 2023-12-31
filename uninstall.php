7e79cc93cab28871dc84e10cb9886e4d42106891
Chandan Dubey <dubeychandan996@gmail.com>
2023-12-31 20:56:28 +0530

    first commit

---
+++ uninstall.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Run the uninstall routine
+ *
+ * @package prop
+ */
+
+// if uninstall.php is not called by WordPress, die.
+if ( ! defined( 'WP_UNINSTALL_PLUGIN' ) ) {
+	die;
+}
+
+delete_option( 'prop-api-key-invalid' );
+delete_option( 'propp_settings' );
